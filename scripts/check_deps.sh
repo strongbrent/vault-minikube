bad6424e0780f243ce9115150a3c5534834ba853
Brent Gawryluik <bgawryluik@appneta.com>
2019-07-03 22:52:39 -0700

    Added check_deps script [skip ci]

---
+++ scripts/check_deps.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+# --- Helper Functions ---------------------------------------------------
+
+# DESC: checks if a specified binary exists in the search path
+# ARGS: $1 (REQ): name of the specified binary
+# OUT: None
+check_binary() {
+
+    if ! command -v "${1}"; then
+        error "ERROR: Can't find ${1} in your PATH"
+        error "Please install ${1}"
+        exit -1
+    fi
+
+    info "${1} is in your PATH"
+}
+
+
+# --- Main Function ------------------------------------------------------
+main() {
+
+    echo ""
+    echo "--- Checking for required binaries ---"
+    local -r deps=(
+        "docker"
+        "kubectl"
+        "minikube"
+        "consul"
+        "vault"
+        "go"
+        "cfssl"
+    )
+
+    # check for required binaries
+    for dep in "${deps[@]}"
+    do
+        check_binary "${dep}"
+    done
+}
